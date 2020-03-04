047a135084bb4c522622a229f9196a8e4ded4908
Ynoa Pedro <ynoa.pedro@outlook.com>
2020-03-04 15:22:58 -0300

    Creats user model

---
+++ Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Apolo.Models
+{
+    public class User
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "Este campo é obrigatório")]
+        public string Name { get; set; }
+    }
+}
