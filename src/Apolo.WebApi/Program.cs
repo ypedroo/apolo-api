b96acdcd487c181ab7dff9b2284e7ad30a93c607
Ynoa Pedro <ynoa.pedro@outlook.com>
2020-03-23 16:03:51 -0300

    Move controller responsabilities to the web api project

--- src/Apolo.WebApi/Program.cs
+++ src/Apolo.WebApi/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace Apolo.WebApi
+namespace apolo
 {
     public class Program
     {
--- src/Apolo.WebApi/Startup.cs
+++ src/Apolo.WebApi/Startup.cs
@@ -1,24 +1,27 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Apolo.Data;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
 
-namespace Apolo.WebApi
+namespace apolo
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services
+                .AddDbContext<DataContext>(opt => opt.UseSqlite("ApoloDB"))
+                .AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Apolo Api", Version = "v1" });
+            });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -27,6 +30,8 @@ namespace Apolo.WebApi
             }
 
             app.UseRouting();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Apolo API")) ;
 
             app.UseEndpoints(endpoints =>
             {
