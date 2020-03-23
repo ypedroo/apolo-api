12e3f2deb354c1758b7a504d2df2a8132f6ae032
Ynoa Pedro <ynoa.pedro@outlook.com>
2020-03-23 18:11:50 -0300

    Create custom controller

--- src/Apolo.WebApi/Controllers/CustomController.cs
+++ src/Apolo.WebApi/Controllers/CustomController.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Net;
+using System.Threading.Tasks;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
@@ -10,6 +13,22 @@ namespace Apolo.WebApi.Controllers
         public CustomController(IMediator mediator)
         {
             _mediator = mediator;
+        }
+
+        public async Task<ActionResult> SendCommand<T>(T command) where T : CommandBase
+        {
+            try
+            {
+                var commandResult = await _mediator.Send(command);
+                if (!commandResult)
+                    return StatusCode((int)HttpStatusCode.BadRequest, commandResult.Exception);
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, ex);
+            }
         }
     }
 }
