44cb67dd001264d7d562654b5c8d09e9c23dbba8
Ynoa Pedro <ynoa.pedro@outlook.com>
2020-03-24 11:30:56 -0300

    Add create song command

--- src/Apolo.Domain/Commands/CreateSongCommand.cs
+++ src/Apolo.Domain/Commands/CreateSongCommand.cs
@@ -1,7 +1,20 @@
+using System;
+using System.Collections.Generic;
+using Apolo.Domain.Models;
+using Apolo.Domain.Core.Commands;
+using Flunt.Validations;
 namespace Apolo.Domain.Commands
 {
-    public class CreateSongCommand
+    public class CreateSongCommand : CommandBase
     {
-        
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public IEnumerable<Songs> LikedBy { get; set; }
+
+        public override void Validate() => AddNotifications(new Contract()
+                    .IsNotEmpty(Id, nameof(Id), $"{nameof(Id)} can't be empty")
+                    .HasMaxLen(Name, 40, "Name", "Name should have no more than 40 chars")
+                    .HasMinLen(Name, 3, "Name", "Name should have at least 3 chars")
+                );
     }
 }
