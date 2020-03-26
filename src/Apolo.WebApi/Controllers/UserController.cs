321b2d3971adf7398d78356d88920687d51bd24b
Ynoa Pedro <ynoa.pedro@outlook.com>
2020-03-26 11:07:01 -0300

    Create user controller
    Co-authored-by: Lucas Lima <lucaslima10077@gmail.com>

---
+++ src/Apolo.WebApi/Controllers/UserController.cs
@@ -0,0 +1,23 @@
+using System.Threading.Tasks;
+using Apolo.Domain.Commands;
+using Apolo.Domain.Interfaces;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Apolo.WebApi.Controllers
+{
+    
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : CustomController
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UserController(IMediator mediator, IUserRepository userRepository) : base(mediator)
+            => _userRepository = userRepository;
+
+        [HttpPost("create")]
+        public async Task<ActionResult> CreateUser(CreateUserCommand createUserCommand)
+            => await SendCommand(createUserCommand);
+    }
+}
